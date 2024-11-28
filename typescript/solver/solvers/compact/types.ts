0ded4619a562892faf91b11beef2edf8011f281a
Tomás Rojas <tmsrjs@gmail.com>
2024-11-28 18:09:46 -0300

    Update The Compact contract types

--- typescript/solver/solvers/compact/types.ts
+++ typescript/solver/solvers/compact/types.ts
@@ -1,15 +1,31 @@
-export type EcoMetadata = {
+export type CompactMetadata = {
   solverName: string;
-  intentSource: {
-    address: string;
-    chainId: number;
-    chainName?: string;
-  };
-  adapters: Array<{
+  arbiters: Array<{
     address: string;
     chainId: number;
     chainName?: string;
   }>;
 };
 
-export type IntentData = { adapter: EcoMetadata["adapters"][number] };
+export type Compact = {
+  hash: string;
+  claimChain: number;
+  compact: {
+    arbiter: string;
+    sponsor: string;
+    nonce: string;
+    expires: string;
+    id: string;
+    amount: string;
+  };
+  intent: {
+    token: string;
+    amount: string;
+    fee: string;
+    chainId: number;
+    recipient: string;
+  };
+  allocatorSignature: string;
+  sponsorSignature: string;
+  filled: boolean;
+};
